af61280a329b2a1af8cb1a24acc67a3500d44c4a
Harshit <harshkush15sep@gmail.com>
2025-11-01 11:00:07 +0530

    final setup missleware and cloudinary

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,30 @@
+import {v2 as cloudinary} from 'cloudinary';
+
+import fs from "fs";
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const uploadImage = async(filePath) => {
+    try{
+        if(!filePath)return null;
+       const response = await cloudinary.uploader.upload(filePath,{
+            resource_type : "auto"
+        })
+        // after upload remove file from server
+        console.log("file has been uploaded on cloudinary", response.url);
+        return response;
+    }
+     catch (eroor){
+        fs.unlinkSync(filePath); // remove the locally saved temporary filed as the upload operation failed
+        console.error("Error while uploading file to cloudinary", eroor);
+        throw eroor;
+    }
+}
+
+export {uploadImage};
+
+    
