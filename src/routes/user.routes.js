e1200e9ed6f2134ce06d03be44cb73787ca04e13
Harshit <harshkush15sep@gmail.com>
2025-11-12 00:48:33 +0530

    feat: reorganize user routes to include change password, user profile, update details, and avatar functionalities

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,16 @@
 import {Router} from "express";
-import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
+import { 
+    changePassword,
+    getCurrentUser, 
+    getUserChannelProfile, 
+    getWatchHistory, 
+    loginUser, 
+    logoutUser, 
+    refreshAccessToken, 
+    registerUser, 
+    updateAvatar, 
+    updateDetails 
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -26,4 +37,10 @@ router.route("/login").post(loginUser)
 router.route("/logout").post(verifyJWT, logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
 
+router.route("/change-password").post(verifyJWT,changePassword)
+router.route("/user-profile").get(verifyJWT, getCurrentUser)
+router.route("/update-details").patch(verifyJWT, updateDetails)
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"),updateAvatar)
+router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+router.route("/watch-history").get(verifyJWT, getWatchHistory)
 export default router;
