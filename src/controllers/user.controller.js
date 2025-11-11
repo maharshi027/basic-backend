926a2091c6c00a5170e6ff9f94b3c75c8c9925d3
Harshit <harshkush15sep@gmail.com>
2025-11-12 00:32:58 +0530

    feat: add getWatchHistory functionality to fetch user watch history with video owner details

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -310,6 +310,57 @@ const getUserChannelProfile = asyncHandler(async (req, res) => {
 
 })
 
+const getWatchHistory = asyncHandler ( async ( req, res) => {
+    // get user watch history controller
+    const user = await User.aggregate([
+        {
+            $match: {
+                _id : req.user?._id
+            }
+        },
+        {
+            $lookup: {
+                from: "videos",
+                localField: "watchHistory",
+                foreignField:"_id",
+                as: "watchHistory",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as : "owner",
+                            pipeline: [
+                                {
+                                    $project: {
+                                        fullName: 1,
+                                        username: 1,
+                                        avatar: 1
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        $addFields:{
+                            owner: { 
+                                $first: "$owner"
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    ])
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(
+            200, user[0].watchHistory, "User watch history fetched successfully"
+        )
+    )
+})
 export {
     registerUser,
     loginUser,
@@ -319,5 +370,6 @@ export {
     getCurrentUser,
     updateDetails,
     updateAvatar,
-    getUserChannelProfile
+    getUserChannelProfile,
+    getWatchHistory
 }
